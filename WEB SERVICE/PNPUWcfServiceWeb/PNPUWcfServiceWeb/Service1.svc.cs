28e2a7d3224be6e759eae524e4adaccf2dedcf41
tbourgois <tbourgois@cegid.com>
2020-03-16 15:58:24 +0100

    TEST

--- WEB SERVICE/PNPUWcfServiceWeb/PNPUWcfServiceWeb/Service1.svc.cs
+++ WEB SERVICE/PNPUWcfServiceWeb/PNPUWcfServiceWeb/Service1.svc.cs
@@ -10,7 +10,8 @@ namespace PNPUWcfServiceWeb
 {
     // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
     // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
-    public class Service1 : IService1
+    //TEST
+	public class Service1 : IService1
     {
         public string LaunchProcess(int value)
         {
