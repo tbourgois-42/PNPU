d7943c7319f5cc666908d77e39d678c3e54edf61
Michael Humbert <michael2.humbert@laposte.net>
2020-03-19 15:56:36 +0100

    Ajout projet PNPUTools et Controle

---
+++ CORE/WcfService1/PNPUCore/ControleTacheSecu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PNPUTools.DataManager;
+using System.Data;
+
+
+namespace PNPUCore.Controle
+{
+    class ControleTacheSecu : IControle
+    {
+        private string sPathMdb = string.Empty;
+
+        ControleTacheSecu(string sPPathMdb)
+        {
+            sPathMdb = sPPathMdb;
+        }
+
+        public bool makeControl()
+        {
+            string sTest;
+            bool bResultat = true;
+
+            DataManagerAccess dmaManagerAccess = null;
+            try
+            {
+                dmaManagerAccess = new DataManagerAccess("D:\\PNPU\\8.1_HF2003_PLFR_PAY.mdb");
+                DataSet dsDataSet = dmaManagerAccess.GetData("select ID_BP FROM M4RBP_DEF WHERE SECURITY_TYPE <> 2");
+                if (dsDataSet.Tables["M4RBP_DEF"].Rows.Count >= 0)
+                {
+                    bResultat = false;
+                    foreach (DataRow drRow in dsDataSet.Tables["M4RBP_DEF"].Rows)
+                    {
+                        sTest = drRow["ID_BP"].ToString() ;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO, loguer l'exception
+                bResultat = false;
+            }
+
+            return bResultat;
+            
+        }
+    }
+}
+
+
